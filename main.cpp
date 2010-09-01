23e69f5a208461a80b1424b3de5deea27132e256
Dawid Holewa <dawidholewa+github@gmail.com>
2010-09-01 13:37:09 +0200

    Dodanie funkcji obslugi klawiatury

--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@
 #include <iostream>
 #include <GL/glut.h>
 
+// Funkcja obslugujaca klawiature
+void klawiatura(unsigned char klawisz, int x, int y) {
+    switch(klawisz) {
+        case 27: // Zamkniecie programu po wcisnieciu klawisza ESC.
+            exit(0);
+            break;
+    }
+}
+
 // Glowna funkcja programu
 int main(int argc, char * argv[]) {
 
