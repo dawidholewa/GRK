5c3db30f30270cc7c31f999ef8cdf68d6353157d
Dawid Holewa <dawidholewa+github@gmail.com>
2010-09-01 14:02:58 +0200

    Dodanie funkcji odpowiedzialnej za skalowanie obrazu wzgledem rozmiaru okna

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 // Created by: Dawid Holewa
 // Date: 01-09-2010
-
 #include <iostream>
 #include <GL/glut.h>
+
+using namespace std;
 
 // Funkcja ustawiajaca podstaweowe ustawienia.
 void init() {
@@ -25,6 +26,31 @@ void wyswietl() {
 
     // Przelaczenie bufforu rysowania ramki.
     glutSwapBuffers();
+}
+
+// Funkcja skalujaca obraz odpowiednio do rozmiaru wyswietlanego okna.
+void skalujObraz(int szer, int wys) {
+
+    // Stosunek szerokosci do wysokosci.
+    GLfloat stosunek = (GLfloat) szer / (GLfloat) wys;
+
+    glViewport(0, 0, szer, wys);
+
+    // Przelaczamy sie na macierz dotyczaca ustawien perspektywy.
+    glMatrixMode(GL_PROJECTION);
+
+    // Resetujemy ustawienia zaladowanej przed chwila macierzy (ladujemy macierz identycznosciowa).
+    glLoadIdentity();
+
+    // Ustawiamy widok odpowiedniej perspektywy.
+    gluPerspective(60, stosunek, 0.5f, 100.0f);
+
+    // Przelaczamy sie na macierz dotyczaca modelu.
+    glMatrixMode(GL_MODELVIEW);
+
+    // Resetujemy ustawienia macierzy modelu.
+    glLoadIdentity();
+
 }
 
 // Funkcja obslugujaca klawiature.
