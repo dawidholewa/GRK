b070adbb3ba5195f0ad7d01cf9a59ff19c3569b0
Dawid Holewa <dawidholewa+github@gmail.com>
2010-09-01 13:50:43 +0200

    Dodanie funkcji obslugujacej wyswietlanie oraz funkcji inicjalizacyjnej

--- main.cpp
+++ main.cpp
@@ -4,7 +4,30 @@
 #include <iostream>
 #include <GL/glut.h>
 
-// Funkcja obslugujaca klawiature
+// Funkcja ustawiajaca podstaweowe ustawienia.
+void init() {
+
+    // Ustawienie koloru tla calej aplikacji.
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+}
+
+// Funkcja obslugujaca rysowanie obrazu wyswietlanego uzytkownikowi.
+void wyswietl() {
+
+    // Czyszczenie buforow.
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // TUTAJ RESZTA KODU !!!
+
+    // Polecenie wykonania wywolanych do tej pory funkcji.
+    glFlush();
+
+    // Przelaczenie bufforu rysowania ramki.
+    glutSwapBuffers();
+}
+
+// Funkcja obslugujaca klawiature.
 void klawiatura(unsigned char klawisz, int x, int y) {
     switch(klawisz) {
         case 27: // Zamkniecie programu po wcisnieciu klawisza ESC.
@@ -13,7 +36,7 @@ void klawiatura(unsigned char klawisz, int x, int y) {
     }
 }
 
-// Glowna funkcja programu
+// Glowna funkcja programu.
 int main(int argc, char * argv[]) {
 
 }
